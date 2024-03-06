93bcdf5b03ebd16eb9ef0c718eef723499ce3780
jrodriguezc160 <jrcortes02102005@educarex.es>
2024-03-06 13:57:38 +0100

    first structure

---
+++ src/components/Post.js
@@ -0,0 +1,41 @@
+import { FiHeart } from "@react-icons/all-files/fi/FiHeart";
+import { FiRepeat } from "@react-icons/all-files/fi/FiRepeat";
+import { FiSend } from "@react-icons/all-files/fi/FiSend";
+import "../index.css"
+
+function Post () {
+  return (
+    <div className='post'>
+
+      <div className='post-profile'>
+        <div className='post-profile-pic'>
+        </div>
+        <div style={{ padding: "8px 0" }}>
+          <b>RODLEYY</b>&nbsp; @rodleyy
+        </div>
+      </div>
+
+      <div className='post-text'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mattis purus et est varius commodo nec eu arcu. Sed scelerisque dapibus diam. Sed blandit vulputate velit, non mattis velit faucibus quis. Nunc id egestas est. Nunc tempus eros mi, sed dignissim orci sodales a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mattis purus et est varius commodo nec eu arcu. Sed scelerisque dapibus diam. Sed blandit vulputate velit, non mattis velit faucibus quis. Nunc id egestas est. Nunc tempus eros mi, sed dignissim orci sodales a.</p>
+      </div>
+
+      <div className='post-icons'>
+        <div className="ic-container">
+          <FiHeart />
+          162
+        </div>
+
+        <div className="ic-container">
+          <FiRepeat />
+          64
+        </div>
+
+        <div className="ic-container">
+          <FiSend />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Post;
--- src/components/PostsPlaceholder.js
+++ src/components/PostsPlaceholder.js
@@ -2,14 +2,17 @@ import { FiTrash } from "@react-icons/all-files/fi/FiTrash";
 import { FiRepeat } from "@react-icons/all-files/fi/FiRepeat";
 import { FiX } from "@react-icons/all-files/fi/FiX";
 import { useEffect, useState } from 'react';
+import Post from './Post';
 
 function PostsPlaceholder () {
 
   return (
-    <div>
+    <div className='posts-placeholder'>
       <h3>POSTS</h3>
 
-
+      <Post />
+      <Post />
+      <Post />
     </div>
   )
 }
