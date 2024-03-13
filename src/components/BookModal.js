6486599882be9135b5319a669da5e3e0bcbb7f22
jrodriguezc160 <jrcortes02102005@educarex.es>
2024-03-13 13:01:54 +0100

    control - modal casi funcional: falta arrays de favoritos y comunicación entre componentes

--- src/components/BookModal.js
+++ src/components/BookModal.js
@@ -1,17 +1,21 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import axios from 'axios';
 import { FiX } from "@react-icons/all-files/fi/FiX";
+import { FiImage } from "@react-icons/all-files/fi/FiImage";
+import { FiStar } from "@react-icons/all-files/fi/FiStar";
+import { FiDelete } from "@react-icons/all-files/fi/FiDelete";
 
 const BookModal = ({ showBookModal, setShowBookModal }) => {
   const [search, setSearch] = useState("");
   const [bookData, setBookData] = useState([]);
+  const inputRef = useRef(null);
 
   const handleCloseModal = () => {
     setShowBookModal(!showBookModal);
   };
 
   const searchBook = () => {
-    axios.get('https://www.googleapis.com/books/v1/volumes?q=' + search + '&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU' + '&maxResults=5')
+    axios.get('https://www.googleapis.com/books/v1/volumes?q=' + search + '&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU' + '&maxResults=20')
       .then(res => setBookData(res.data.items))
       .catch(err => console.log(err))
   }
@@ -22,12 +26,22 @@ const BookModal = ({ showBookModal, setShowBookModal }) => {
     searchBook();
   }
 
+  const handleClearInput = () => {
+    setSearch('');
+  }
+
   return (
     <div className={`modal-screen ${showBookModal ? 'visible' : ''}`} >
       <div className="modal">
         <div className="search-bar">
-          <input type="text" name="book-search" id="book-search" placeholder='Search for your favourite books...' value={search} onChange={handleInputChange} />
-          <div className="ic-container" onClick={handleCloseModal} style={{ marginRight: "0px" }} >
+          <div className='search-bar-input'>
+            <input ref={inputRef} type="text" name="book-search" id="book-search" placeholder='Search for your favourite books...' value={search} onChange={handleInputChange} />
+            <div className='ic-container' onClick={handleClearInput}>
+              <FiDelete />
+            </div>
+          </div>
+
+          <div className="ic-container" onClick={handleCloseModal} >
             <FiX />
           </div>
         </div>
@@ -36,10 +50,18 @@ const BookModal = ({ showBookModal, setShowBookModal }) => {
           {bookData.map((book, index) => (
             <div key={index} className='book-result'>
               <div className='book-cover'>
-                <img src={book.volumeInfo.imageLinks?.thumbnail} />
+                {book.volumeInfo.imageLinks?.thumbnail ? (
+                  <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />
+                ) : (
+                  <div style={{ width: '50%', height: '50%', color: 'lightgray', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <FiImage />
+                  </div>
+                )}
               </div>
-
               <p>{book.volumeInfo.title}</p>
+              <div className='ic-container'>
+                <FiStar />
+              </div>
             </div>
           ))}
         </div>
