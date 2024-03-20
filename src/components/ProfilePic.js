9da1cf8023929d8c61c7eec624172730b31e9549
jrodriguezc160 <jrcortes02102005@educarex.es>
2024-03-20 14:57:21 +0100

    control

--- src/components/ProfilePic.js
+++ src/components/ProfilePic.js
@@ -1,33 +1,45 @@
 import { FiFilePlus } from "@react-icons/all-files/fi/FiFilePlus";
 import { FiEdit2 } from "@react-icons/all-files/fi/FiEdit2";
 import "../index.css"
+import { useState } from 'react';
 
 function ProfilePic ({ setShowProfilePicModal, profilePic }) {
+  const [iconVisible, setIconVisible] = useState(false)
 
   const handleClick = () => {
     setShowProfilePicModal(true)
   }
 
-  return (
-    <div style={{ display: "flex", alignItems: "center", marginTop: "32px" }}>
-      <div className="ic-container" style={{ position: 'relative', zIndex: '999' }}>
-        <FiEdit2 />
-      </div>
-      <div className='profile-pic' style={{ marginRight: "64px" }}>
-        {!profilePic
-          ? (<div className='ic-container' style={{ height: "32px", width: "32px" }} onClick={handleClick}>
-            <FiFilePlus />
-          </div>)
-          : (<img src={profilePic} style={{ width: 'inherit', height: 'inherit' }} />)}
-      </div>
+  const handleMouseEnter = () => {
+    setIconVisible(true);
+  };
 
+  const handleMouseLeave = () => {
+    setIconVisible(false);
+  };
+
+
+  return (
+    <div style={{ display: "flex", alignItems: "center", marginTop: "32px" }} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div style={{ position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'center', width: '8rem', height: '8rem', marginRight: '4rem' }}>
+        <div className={`ic-container`} style={{ position: 'absolute', top: 0, right: 0, zIndex: '999' }}>
+          <FiEdit2 />
+        </div>
+        <div className='profile-pic'>
+          {!profilePic
+            ? (<div className='ic-container' style={{ height: "32px", width: "32px" }} onClick={handleClick}>
+              <FiFilePlus />
+            </div>)
+            : (<img src={profilePic} style={{ width: 'inherit', height: 'inherit' }} />)}
+        </div>
+      </div>
       <div className='profile-text'>
         <h1>RODLEYY</h1>
         <h3 style={{ fontWeight: "normal" }}>@rodleyy</h3>
       </div>
-    </div>
+    </div >
   )
-
 }
+
 
 export default ProfilePic;
