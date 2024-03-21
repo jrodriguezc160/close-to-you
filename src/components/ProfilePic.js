808220182f8517f208ba643680769cfa6a0093bb
jrodriguezc160 <jrcortes02102005@educarex.es>
2024-03-21 08:30:49 +0100

    profile picture working

--- src/components/ProfilePic.js
+++ src/components/ProfilePic.js
@@ -22,7 +22,7 @@ function ProfilePic ({ setShowProfilePicModal, profilePic }) {
   return (
     <div style={{ display: "flex", alignItems: "center", marginTop: "32px" }} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       <div style={{ position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'center', width: '8rem', height: '8rem', marginRight: '4rem' }}>
-        <div className={`ic-container`} style={{ position: 'absolute', top: 0, right: 0, zIndex: '999' }}>
+        <div className={`ic-container ${iconVisible === true && profilePic !== '' ? ('visible') : ('hidden-icon')}`} style={{ position: 'absolute', top: 0, right: 0, zIndex: '999' }}>
           <FiEdit2 />
         </div>
         <div className='profile-pic'>
