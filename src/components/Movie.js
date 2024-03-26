397bd49ce3026f247ee84566f3f7bc1bb4cf742f
jrodriguezc160 <jrcortes02102005@educarex.es>
2024-03-26 14:56:38 +0100

    ux decission

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -28,58 +28,54 @@ const Movie = ({ movie, index, myMovies, myFavMovies, handleAddFavourite, handle
 
   return (
     <div className="movie" onMouseEnter={() => handleOnHover(movie.title)} onMouseLeave={() => handleHoverLeave()}>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <div>
-
-
-          <div key={index} className='cover'>
-            {movie.poster_path ? (
-              <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} style={{ zIndex: '2' }} />
-            ) : (
-              <div style={{ width: '100%', height: '100%', color: 'lightgray', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <FiImage />
-              </div>
-            )}
-
-            {onHover === movie.title ? (
-              <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className={`ambilight ${addVisible === true ? 'visible' : ''}`} />
-            ) : ('')}
+      <div key={index} className='cover'>
+        {movie.poster_path ? (
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} style={{ zIndex: '2' }} />
+        ) : (
+          <div style={{ width: '100%', height: '100%', color: 'lightgray', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <FiImage />
           </div>
+        )}
 
-          <div style={{ display: 'flex', justifyContent: 'center', gap: '8px' }}>
-            <div className='ic-container' >
-              <FiStar
-                onClick={() => {
-                  if (!myFavMovies.some(favMovie => favMovie.id === movie.id)) {
-                    handleAddFavourite(movie);
-                  } else {
-                    handleRemoveFavourite(movie);
-                  }
-                }}
-                fill={myFavMovies.some(favMovie => favMovie.id === movie.id) ? 'gray' : 'none'}
-              />
-            </div>
-            <div className='ic-container' >
-              {!myMovies.some(favMovie => favMovie.id === movie.id) ? (
-                <FiPlusCircle
-                  onClick={() => handleAddMovie(movie)}
-                  stroke='gray'
-                />
-              ) : (
-                <FiCheckCircle
-                  onClick={() => handleRemoveMovie(movie)}
-                  stroke='gray'
-                />
-              )
+        {onHover === movie.title ? (
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className={`ambilight ${addVisible === true ? 'visible' : ''}`} />
+        ) : ('')}
+      </div>
+
+      <div style={{ display: 'flex', justifyContent: 'center', gap: '8px' }}>
+        <div className='ic-container' >
+          <FiStar
+            onClick={() => {
+              if (!myFavMovies.some(favMovie => favMovie.id === movie.id)) {
+                handleAddFavourite(movie);
+              } else {
+                handleRemoveFavourite(movie);
               }
-            </div>
-          </div>
+            }}
+            fill={myFavMovies.some(favMovie => favMovie.id === movie.id) ? 'gray' : 'none'}
+          />
         </div>
-        <div className="text">
-          <h3>{movie.title}</h3>
-          {movie.overview}
+        <div className='ic-container' >
+          {!myMovies.some(favMovie => favMovie.id === movie.id) ? (
+            <FiPlusCircle
+              onClick={() => handleAddMovie(movie)}
+              stroke='gray'
+            />
+          ) : (
+            <FiCheckCircle
+              onClick={() => handleRemoveMovie(movie)}
+              stroke='gray'
+            />
+          )
+          }
         </div>
       </div>
+      {/*
+      <div className="text">
+          <h3 style={{ height: 'fit-content', padding: '0', marginTop: '0' }}>{movie.title}</h3>
+          {movie.overview}
+        </div>
+        */}
     </div>
   )
 }
