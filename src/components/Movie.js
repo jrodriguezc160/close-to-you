ea2c30fa5bfe167204d32ec3b6f9a6c74490b637
jrodriguezc160 <jrcortes02102005@educarex.es>
2024-03-26 14:59:00 +0100

    control

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,6 +4,7 @@ import { FiPlusCircle } from "@react-icons/all-files/fi/FiPlusCircle";
 import { FiCheckCircle } from "@react-icons/all-files/fi/FiCheckCircle";
 import { FiImage } from "@react-icons/all-files/fi/FiImage";
 import '../styles/favourites.css'
+import { Tooltip } from '@mui/material';
 
 const Movie = ({ movie, index, myMovies, myFavMovies, handleAddFavourite, handleRemoveFavourite, handleAddMovie, handleRemoveMovie }) => {
   const [addVisible, setAddVisible] = useState(false);
@@ -28,54 +29,56 @@ const Movie = ({ movie, index, myMovies, myFavMovies, handleAddFavourite, handle
 
   return (
     <div className="movie" onMouseEnter={() => handleOnHover(movie.title)} onMouseLeave={() => handleHoverLeave()}>
-      <div key={index} className='cover'>
-        {movie.poster_path ? (
-          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} style={{ zIndex: '2' }} />
-        ) : (
-          <div style={{ width: '100%', height: '100%', color: 'lightgray', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <FiImage />
-          </div>
-        )}
-
-        {onHover === movie.title ? (
-          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className={`ambilight ${addVisible === true ? 'visible' : ''}`} />
-        ) : ('')}
-      </div>
-
-      <div style={{ display: 'flex', justifyContent: 'center', gap: '8px' }}>
-        <div className='ic-container' >
-          <FiStar
-            onClick={() => {
-              if (!myFavMovies.some(favMovie => favMovie.id === movie.id)) {
-                handleAddFavourite(movie);
-              } else {
-                handleRemoveFavourite(movie);
-              }
-            }}
-            fill={myFavMovies.some(favMovie => favMovie.id === movie.id) ? 'gray' : 'none'}
-          />
-        </div>
-        <div className='ic-container' >
-          {!myMovies.some(favMovie => favMovie.id === movie.id) ? (
-            <FiPlusCircle
-              onClick={() => handleAddMovie(movie)}
-              stroke='gray'
-            />
+      <Tooltip title="You don't have permission to do this" followCursor>
+        <div key={index} className='cover'>
+          {movie.poster_path ? (
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} style={{ zIndex: '2' }} />
           ) : (
-            <FiCheckCircle
-              onClick={() => handleRemoveMovie(movie)}
-              stroke='gray'
-            />
-          )
-          }
+            <div style={{ width: '100%', height: '100%', color: 'lightgray', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <FiImage />
+            </div>
+          )}
+
+          {onHover === movie.title ? (
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className={`ambilight ${addVisible === true ? 'visible' : ''}`} />
+          ) : ('')}
         </div>
-      </div>
-      {/*
+
+        <div style={{ display: 'flex', justifyContent: 'center', gap: '8px' }}>
+          <div className='ic-container' >
+            <FiStar
+              onClick={() => {
+                if (!myFavMovies.some(favMovie => favMovie.id === movie.id)) {
+                  handleAddFavourite(movie);
+                } else {
+                  handleRemoveFavourite(movie);
+                }
+              }}
+              fill={myFavMovies.some(favMovie => favMovie.id === movie.id) ? 'gray' : 'none'}
+            />
+          </div>
+          <div className='ic-container' >
+            {!myMovies.some(favMovie => favMovie.id === movie.id) ? (
+              <FiPlusCircle
+                onClick={() => handleAddMovie(movie)}
+                stroke='gray'
+              />
+            ) : (
+              <FiCheckCircle
+                onClick={() => handleRemoveMovie(movie)}
+                stroke='gray'
+              />
+            )
+            }
+          </div>
+        </div>
+        {/*
       <div className="text">
           <h3 style={{ height: 'fit-content', padding: '0', marginTop: '0' }}>{movie.title}</h3>
           {movie.overview}
         </div>
         */}
+        <Tooltip />
     </div>
   )
 }
